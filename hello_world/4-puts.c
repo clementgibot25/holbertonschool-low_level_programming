dc2f600a677affa97096e5632083836b8e8d88d8
clementgibot25 <10500@holbertonstudents.com>
2025-02-14 12:56:02 +0100

    e

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ * main - beginning
+ *
+ * return: always 0
+ */
 
 	int main(void)
 	{
 		puts("\"Programming is like building a multilingual puzzle");
-		return 0;
+		return (0);
 	}
