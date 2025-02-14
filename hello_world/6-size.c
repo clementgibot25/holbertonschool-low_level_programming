f0425a3dbdfb9d95d86477f0fc082ef3844c70da
clementgibot25 <10500@holbertonstudents.com>
2025-02-14 14:43:23 +0100

    e

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
@@ -12,6 +12,6 @@ int main(void)
 	printf("size of a int: %lu bytes(s)\n",sizeof(int));
 	printf("size of a long int: %lu byte(s)\n",sizeof(long int));
 	printf("size of a long long int: %lu byte(s)\n",sizeof(long long int));
-	  printf("size of a float: %lu byte(s)\n",sizeof(float));
-	 return (0);
-  }
+	printf("size of a float: %lu byte(s)\n",sizeof(float));
+		return (0);
+}
