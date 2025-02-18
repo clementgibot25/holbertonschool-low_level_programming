c6f7496815fa2e6bc53f1d765d0700d73c37cc26
clementgibot25 <10500@holbertonstudents.com>
2025-02-18 13:36:13 +0100

    alphabet in lowercase

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ * 	followed by a new line
+ */
+void print_alphabet(void)
+{
+	for letter ( letter == 'a'; letter <= 'z'; letter++ );
+		_putchar(letter);
+}
