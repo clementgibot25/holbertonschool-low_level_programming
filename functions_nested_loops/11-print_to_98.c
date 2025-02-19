f14e3189e302b895625927c76429c5a17f01dd5a
clementgibot25 <10500@holbertonstudents.com>
2025-02-19 15:07:18 +0100

    added exercice 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ *
+ * @n: first number given
+ *
+ * return: 0
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 97)
+	{
+		for (; n <= 97; n++)
+		{
+			printf("%d ,", n);
+		}
+	}
+	else if (n >= 97)
+	{
+	for (; n >= 97; n--)
+		{
+			printf("%d ,", n);
+		}
+	}
+	printf("%d ,", n);
+}
