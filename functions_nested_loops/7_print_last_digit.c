c1dbe9470dc81fbd0a49fcfbd021517881f9c2d6
clementgibot25 <10500@holbertonstudents.com>
2025-02-19 11:54:09 +0100

    added exercice 7

---
+++ functions_nested_loops/7_print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*
+ * print_last_digit - print the last digit of a number
+ *
+ * @n: the number we searching the last digit of
+ *
+ * Return: i
+ */
+int print_last_digit(int n)
+{
+	int i = n % 10;
+
+	return (i);
+}
