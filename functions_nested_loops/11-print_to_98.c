e284cd3fd50a61c9c07b3c54b3899cb9bea0146a
clementgibot25 <10500@holbertonstudents.com>
2025-02-19 15:15:35 +0100

    added exercice 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,16 +13,17 @@ void print_to_98(int n)
 	{
 		for (; n < 97; n++)
 		{
-			printf("%d ,", n);
+			printf("%d, ", n);
 		}
+	printf("98\n");
 	}
 	else if (n > 97)
 	{
 	for (; n > 97; n--)
 		{
-			printf("%d ,", n);
+			printf("%d, ", n);
 		}
-	printf("%d ,", n);
+	printf("98\n");
 	}
 	else
 	printf("98\n");
