57c75d4737b22a9eb0dc83eca21664eb06b951ef
clementgibot25 <10500@holbertonstudents.com>
2025-02-24 11:41:53 +0100

    added exercice 5

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -8,10 +8,10 @@
  * 	 used in the more_functions_nested_loops directory.
  */
 
- int _isupper(int c);
+int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 int _putchar(char c);
-
+void print_most_numbers(void);
  #endif
