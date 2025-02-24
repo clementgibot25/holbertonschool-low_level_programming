875d437a373847b71d7007b43043adc958cd3587
clementgibot25 <10500@holbertonstudents.com>
2025-02-24 11:21:18 +0100

    print alphabet

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _mul - multiplie two integers
+ * mul - multiplie two integers
  * @a: first integer
  * @b: second integer
  *
  * return: return of the multiplication
  */
 
-int _mul(int a, int b)
+int mul(int a, int b)
 {
 	return a * b;
 }
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -10,5 +10,5 @@
 
  int _isupper(int c);
 int _isdigit(int c);
-int _mul(int a, int b);
+int mul(int a, int b);
  #endif
