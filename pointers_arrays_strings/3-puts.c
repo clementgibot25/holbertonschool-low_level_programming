55b8f830f0c32256b156e96baad0b41658990b88
clementgibot25 <10500@holbertonstudents.com>
2025-02-25 14:11:30 +0100

    adding directory of the new project

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed
+ * by a new line, to stdout
+ *
+ */
+
+ void _puts(char *str)
+ {
+    char *chaine = *str;
+    _putchar("%s\n", chaine);
+ }
