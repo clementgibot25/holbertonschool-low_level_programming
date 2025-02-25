7e75bb4679201f1b2da440311f222a335a0ccfbf
clementgibot25 <10500@holbertonstudents.com>
2025-02-25 14:52:18 +0100

    added exercice 2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string to check
- *
- * Return: length of the string
- */
+* _strlen - returns the length of a string
+* @s: string to check
+*
+* Return: length of the string
+*/
 
- int _strlen(char *s)
- {
-     int i = 0;
+int _strlen(char *s)
+{
+int i = 0;
 
-     while (s[i] != '\0')
-     {
-         i++;
-     }
-     return (i);
- }
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+}
