152300f5c2a6a96dd9af7f04df1c106ef446a83e
clementgibot25 <10500@holbertonstudents.com>
2025-02-25 14:26:30 +0100

    adding directory of the new project

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -8,12 +8,6 @@
 
  void _puts(char *str)
  {
-     int i = 0;
-
-     while (str[i] != '\0')
-     {
-         _putchar(str[i]);
-         i++;
-     }
-     _putchar('\n');
+    char chaine = *str;
+    _putchar("%s\n", chaine);
  }
