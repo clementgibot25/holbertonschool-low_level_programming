7906406f2b0d8da19e90ea8ff0fb6c28411e42a2
clementgibot25 <10500@holbertonstudents.com>
2025-02-25 14:35:15 +0100

    fixing exercice 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -3,11 +3,14 @@
 /**
  * _puts - prints a string, followed
  * by a new line, to stdout
- *
+ *@str: the string to print
  */
-
- void _puts(char *str)
- {
-    char chaine = *str;
-    _putchar("%s\n", chaine);
- }
+void _puts(char *str)
+{
+    while (*str)
+    {
+        _putchar(*str);
+        str++;
+    }
+    _putchar('\n');
+}
