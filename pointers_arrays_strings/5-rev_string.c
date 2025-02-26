ecd5eb8d7d15cc9addb06aab766ea6db6c5bb8e2
clementgibot25 <10500@holbertonstudents.com>
2025-02-26 11:02:09 +0100

    added exo 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string
+ * @s: string that will be reversed
+ *
+ */
+void rev_string(char *s)
+{
+    int a = 0;
+    int b = _strlen(s) - 1;
+    char c;
+
+    while (a < b)
+    {
+        c = s[a];
+        s[a] = s[b];
+        s[b] = c;
+
+        a++;
+        b++;
+    }
+}
