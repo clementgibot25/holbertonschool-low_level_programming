751dbbd56785b2e11aa12137df27771fa0cfe980
clementgibot25 <10500@holbertonstudents.com>
2025-02-26 16:02:35 +0100

    added exercice 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,25 +8,25 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    int result = 0;
-    int i = 0;
+	int sign = 1;
+	int result = 0;
+	int i = 0;
 
-    while (s[i] == ' ')
-        i++;
+	while (s[i] == ' ')
+		i++;
 
-    if (s[i] == '-' || s[i] == '+')
-    {
-        if (s[i] == '-')
-            sign = -1;
-        i++;
-    }
+	if (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			sign = -1;
+		i++;
+	}
 
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        result = result * 10 + (s[i] - '0');
-        i++;
-    }
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
 
-    return result * sign;
+	return result * sign;
 }
