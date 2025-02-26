3b69410ab8798558881393bf567e5b36186268cb
clementgibot25 <10500@holbertonstudents.com>
2025-02-26 14:30:29 +0100

    added exercice 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -8,15 +8,15 @@
  */
 void print_array(int *a, int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < n; i++)
-    {
-        _putchar("%d", a[i]);
-        if (i < n - 1)
-        {
-            _putchar(", ");
-        }
-    }
-    _putchar("\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
 }
