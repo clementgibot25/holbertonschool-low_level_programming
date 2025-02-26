ce0fdcd9146889eec918d49f6401562a738b9242
clementgibot25 <10500@holbertonstudents.com>
2025-02-26 11:46:03 +0100

    added exo 5 and updated main.h

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts2 - function that prints every other character of a string,
+ * starting with the first character, followed by a new line
+ * @str: string printed
+ */
+
+ void puts2(char *str)
+ {
+	 int i = 0;
+	 int len = _strlen(str);
+
+	 while (i < len)
+	 {
+		 if (i % 2 == 0)
+		 {
+			 _putchar(str[i]);
+		 }
+		 i++;
+	 }
+	 _putchar('\n');
+ }
