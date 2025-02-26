be9b019b7c4124ca6f594f2cff37ed9a8dd1da0c
clementgibot25 <10500@holbertonstudents.com>
2025-02-26 15:38:21 +0100

    fixing exercice 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -11,11 +11,11 @@
 
 char *_strcpy(char *dest, char *src)
 {
-    int i;
-    int n = _strlen(src);
-    for (i = 0; i <= n; i++)
-    {
-        dest[i] = src[i];
-    }
-    return (dest);
+	int i;
+	int n = _strlen(src);
+	for (i = 0; i <= n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
