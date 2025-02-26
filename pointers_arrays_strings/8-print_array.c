6447d31e5b77d5cef83bf46978499a5a94ed857a
clementgibot25 <10500@holbertonstudents.com>
2025-02-26 14:20:07 +0100

    added exercice 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - prints 'n' elements of array of integers
+ * @a: the array of integers
+ * @n: the number of elements to print
+ */
+void print_array(int *a, int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        _putchar("%d", a[i]);
+        if (i < n - 1)
+        {
+            _putchar(", ");
+        }
+    }
+    _putchar("\n");
+}
