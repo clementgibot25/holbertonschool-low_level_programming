c6738106efeec375c1ea9aa383b7b2ff8aa47b71
clementgibot25 <10500@holbertonstudents.com>
2025-02-26 11:47:20 +0100

    added exo 5 and updated main.h

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,18 +7,18 @@
  * @str: string printed
  */
 
- void puts2(char *str)
- {
-	 int i = 0;
-	 int len = _strlen(str);
+void puts2(char *str)
+{
+	int i = 0;
+	int len = _strlen(str);
 
-	 while (i < len)
-	 {
-		 if (i % 2 == 0)
-		 {
-			 _putchar(str[i]);
-		 }
-		 i++;
-	 }
-	 _putchar('\n');
- }
+	while (i < len)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
