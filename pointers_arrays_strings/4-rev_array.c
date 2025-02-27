91740010bf38dbe7a75e38180816d333870783ad
clementgibot25 <10500@holbertonstudents.com>
2025-02-27 15:10:44 +0100

    added exercice 4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * reverse_array - function that reverses an array of integers
+ * @a: array that will be reversed
+ * @len: length of the array
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int b = n - 1;
+	int temp;
+
+	while (i < b)
+	{
+		n = a[i];
+		a[i] = a[b];
+		a[b] = temp;
+
+		i++;
+		b--;
+	}
+}
