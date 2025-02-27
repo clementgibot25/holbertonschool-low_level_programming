9bdb5ee9577b9946a76bda209aae38b6951408fc
clementgibot25 <10500@holbertonstudents.com>
2025-02-27 13:27:12 +0100

    added exercice 1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -16,7 +16,8 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[i] != '\0')
 		i++;
 
-	while (j < n && src[j] != '\0') {
+	while (j < n && src[j] != '\0')
+	{
 		dest[i] = src[j];
 		i++;
 		j++;
@@ -24,5 +25,5 @@ char *_strncat(char *dest, char *src, int n)
 
 	dest[i] = '\0';
 
-	return dest;
+	return (dest);
 }
