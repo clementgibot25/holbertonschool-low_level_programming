b87ee7841970056ef489aaf5b4fd9f1837a262ca
clementgibot25 <10500@holbertonstudents.com>
2025-02-27 10:54:30 +0100

    updated exercice 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,32 +1,32 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
+ * atoi - converts a string to an integer with error handling
+ * @str: string to be converted
+ * Return: the int converted from the string, or 0 if no valid number is found
  */
-int _atoi(char *s)
+int atoi(char *str)
 {
-	int sign = 1;
-	int result = 0;
-	int i = 0;
+	int sign = 1, num = 0, idx = 0;
+	int has_digit = 0;
 
-	while (s[i] == ' ')
-		i++;
+	while (str[idx] == ' ')
+		idx++;
 
-	if (s[i] == '-' || s[i] == '+')
-	{
-		if (s[i] == '-')
+	if (str[idx] == '-' || str[idx] == '+') {
+		if (str[idx] == '-')
 			sign = -1;
-		i++;
+		idx++;
 	}
 
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		result = result * 10 + (s[i] - '0');
-		i++;
+	while (str[idx] >= '0' && str[idx] <= '9') {
+		num = num * 10 + (str[idx] - '0');
+		idx++;
+		has_digit = 1;
 	}
 
-	return result * sign;
+	if (has_digit)
+		return num * sign;
+	else
+		return 0;
 }
