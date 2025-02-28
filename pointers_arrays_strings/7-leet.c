f83ae361d6d477eb1314a815fa9b735e39156c87
clementgibot25 <10500@holbertonstudents.com>
2025-02-28 15:22:22 +0100

    trying to fix exo 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -7,15 +7,15 @@ char *leet(char *s)
 {
 	int i = 0;/*parcoureur de tableau */
 	int j;/*boucler tableau*/
-	char min[] = {'a','e', 'o', 't', 'l'};
+	char min[] = {'a', 'e', 'o', 't', 'l'};
 	char maj[] = {'A', 'E', 'O', 'T', 'L'};
 	char num[] = {4, 3, 0, 7, 1};
 
-	while(s[i] != '\0')
+	while (s[i] != '\0')
 	{
 		for (j = 0; j < 5; j++)
 		{
-			if(s[i] == min[j] || s[i] == maj[j])
+			if (s[i] == min[j] || s[i] == maj[j])
 				{
 					s[i] = num[j] + '0';
 				}
