95b49c777c5bd6b7390da2656ef4abf085a8ec81
clementgibot25 <10500@holbertonstudents.com>
2025-02-28 14:24:31 +0100

    added exrcice 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *
+ */
+
+char *leet(char *s)
+{
+	int i = 0;/*parcoureur de tableau */
+	int j = 0;/*boucler tableau*/
+	char min[] = {'a','e', 'o', 't', 'l'};
+	char maj[] = {'A', 'E', 'O', 'T', 'L'};
+	char num[] = {4, 3, 0, 7, 1};
+
+	while(s[i] != '\0')
+	{
+	i++;
+	for (i = 1; s[i] != '\0'; i++)
+	if(s[i] == min[i] || s[i] == maj[i])
+		{
+			s[i] = num[j];
+			j++;
+		}
+	}
+	return (s);
+}
