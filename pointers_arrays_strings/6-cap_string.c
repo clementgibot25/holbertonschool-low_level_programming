b8ebdb1f2020ba30a2407743bf35564fba7e28a7
clementgibot25 <10500@holbertonstudents.com>
2025-02-28 12:11:28 +0100

    added exercice 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,27 +1,34 @@
 #include "main.h"
 
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @s: The string to modify.
+ *
+ * Return: The modified string.
+ */
 char *cap_string(char *s)
 {
-		int i;
+	int i;
 
-	if(s[0]>=97&&s[0]<=122)
+	if (s[0] >= 'a' && s[0] <= 'z')
 	{
-		s[0]-=32;
+		s[0] -= 32;
 	}
-	for(i=1;s[i]!='\0';i++)
+
+	for (i = 1; s[i] != '\0'; i++)
 	{
-		if(s[i-1]== ' ' || s[i-1]== ',' || s[i-1]== ';' ||
-			s[i-1]== '.' || s[i-1]== '!' || s[i-1]== '?' ||
-			s[i-1]== '"' || s[i-1]== '(' || s[i-1]== ')' ||
-			s[i-1]== '{' || s[i-1]== '}' || s[i-1] == '\t' ||
-			s[i-1] == '\n')
+		if (s[i - 1] == ' ' || s[i - 1] == ',' || s[i - 1] == ';' ||
+		    s[i - 1] == '.' || s[i - 1] == '!' || s[i - 1] == '?' ||
+		    s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')' ||
+		    s[i - 1] == '{' || s[i - 1] == '}' || s[i - 1] == '\t' ||
+		    s[i - 1] == '\n')
 		{
-				if(s[i]>=97 &&s[i]<=122)
-				{
-					 s[i]-=32;
-				}
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] -= 32;
+			}
 		}
 	}
 
-	return s;
+	return (s);
 }
