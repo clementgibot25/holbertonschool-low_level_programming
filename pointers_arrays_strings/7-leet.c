97970049520a2cf712c7d33ec69ca15f1a2deac9
clementgibot25 <10500@holbertonstudents.com>
2025-02-28 15:53:19 +0100

    fixed exo 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,22 +8,22 @@
  */
 char *leet(char *s)
 {
-    int i = 0; /* parcoureur de tableau */
-    int j; /* boucler tableau */
-    char min[] = {'a', 'e', 'o', 't', 'l'};
-    char maj[] = {'A', 'E', 'O', 'T', 'L'};
-    char num[] = {4, 3, 0, 7, 1};
+	int i = 0; /* parcoureur de tableau */
+	int j; /* boucler tableau */
+	char min[] = {'a', 'e', 'o', 't', 'l'};
+	char maj[] = {'A', 'E', 'O', 'T', 'L'};
+	char num[] = {4, 3, 0, 7, 1};
 
-    while (s[i] != '\0')
-    {
-        for (j = 0; j < 5; j++)
-        {
-            if (s[i] == min[j] || s[i] == maj[j])
-            {
-                s[i] = num[j] + '0';
-            }
-        }
-        i++;
-    }
-    return (s);
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == min[j] || s[i] == maj[j])
+			{
+				s[i] = num[j] + '0';
+			}
+		}
+		i++;
+	}
+	return (s);
 }
