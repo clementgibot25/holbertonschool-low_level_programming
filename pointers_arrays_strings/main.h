c35bf88d185dba73deb048fbf06329d7dae412b6
clementgibot25 <10500@holbertonstudents.com>
2025-03-04 10:23:17 +0100

    added exercice 2

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define NULL ((void *)0)
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -23,6 +25,5 @@ char *cap_string(char *s);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-#define NULL ((void *)0);
 
 #endif
