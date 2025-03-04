91344c6a09f56f0984969b341449db7904103863
clementgibot25 <10500@holbertonstudents.com>
2025-03-04 10:48:10 +0100

    added exercice 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * _strspn - gets the length of a prefix substring
+ * _strpbrk - gets the length of a prefix substring
  * @s: string to check
  * @accept: string containing the characters to match
  *
  * Return: number of bytes in the initial segment of s
  * which only contain characters from accept
  */
-unsigned int _strspn(char *s, char *accept)
+unsigned int _strpbrk(char *s, char *accept)
 {
 	unsigned int i = 0;
-	while (s[i] != '\0' && strchr(accept, s[i]) != NULL)
+	while (s[i] != '\0' && _strpbrk(accept, s[i]) != NULL)
 		i++;
 	return (i);
 }
