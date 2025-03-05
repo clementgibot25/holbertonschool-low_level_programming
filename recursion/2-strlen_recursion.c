5e76b29edfd2332ace145bcfff8cccc6f4c6591c
clementgibot25 <10500@holbertonstudents.com>
2025-03-05 14:23:27 +0100

    fixing exo 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _str_len_recursion - returns the length of a string
+ * _strlen_recursion - returns the length of a string
  * @s: the string
  * Return: the length of the string
  */
-int _str_len_recursion(char *s)
+int _strlen_recursion(char *s)
 {
     if (*s == '\0')
         return (0);
 
-    return (1 + _str_len_recursion(s + 1));
+    return (1 + _strlen_recursion(s + 1));
 }
