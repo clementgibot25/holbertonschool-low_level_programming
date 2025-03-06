21f7ae029f8df8050089d60576236edd58a9d4b0
clementgibot25 <10500@holbertonstudents.com>
2025-03-06 11:55:23 +0100

    fixing last exo

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -7,9 +7,27 @@
  */
 int is_prime_number(int n)
 {
+    if (n <= 1)
+        return (0);
     if (n == 2)
         return (1);
-    if (n % 2 == 0 || n <= 1)
+    if (n % 2 == 0)
         return (0);
-    return (is_prime_number(n - 2));
+    return (check_odd_factors(n, 3));
+}
+
+/**
+ * check_odd_factors - checks if a number has an odd factor
+ * @n: number to check
+ * @i: current number to check
+ *
+ * Return: 1 if no odd factor, 0 if odd factor
+ */
+int check_odd_factors(int n, int i)
+{
+    if (i * i > n)
+        return (1);
+    if (n % i == 0)
+        return (0);
+    return (check_odd_factors(n, i + 2));
 }
