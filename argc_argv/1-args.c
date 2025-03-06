eb0c50936ef1fc473a4ac20ab5396e918cb17d78
clementgibot25 <10500@holbertonstudents.com>
2025-03-06 15:44:12 +0100

    added exo 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: Number of arguments passed to the program.
+ * @argv: Array of pointers to the arguments passed to the program.
+ *
+ * Return: Always 0.
+ */
+int main( int argc, char *argv[] )
+{
+    (void)argv;
+    printf("%d\n", argc - 1);
+    return (0);
+}
