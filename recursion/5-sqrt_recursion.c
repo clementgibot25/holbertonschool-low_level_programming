374b1b16fced1949c75ed21228d0160021db815c
clementgibot25 <10500@holbertonstudents.com>
2025-03-06 10:38:37 +0100

    added exercice 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -8,15 +8,19 @@
  */
 int _sqrt_recursion(int n)
 {
-    static int i = 0;
-
     if (n < 0)
         return -1;
+    if (n == 0 || n == 1)
+        return n;
+    return _sqrt_recursion_iteration(n, 1);
+}
+int _sqrt_recursion_iteration(int n, int i)
+{
     if (i * i == n)
         return i;
     if (i * i > n)
         return -1;
 
     i++;
-    return _sqrt_recursion(n);
+    return _sqrt_recursion_iteration(n, i);
 }
