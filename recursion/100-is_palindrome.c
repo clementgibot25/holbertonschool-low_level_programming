de45aaa4e2cf6da88647a3c19dbc235a5c3eee47
clementgibot25 <10500@holbertonstudents.com>
2025-03-06 12:08:19 +0100

    added extra exo

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -7,22 +7,34 @@
  */
 int is_palindrome(char *s)
 {
-    int len = 0;
-    char *end = s;
-
-    while (*end != '\0') {
-        len++;
-        end++;
-    }
-
-    return _is_palindrome(s, 0, len - 1);
+    int len = _strlen_recursion(s);
+    return check_pal(s, 0, len - 1);
 }
 
-int _is_palindrome(char *s, int i, int len)
+/**
+ * check_pal - checks if a string is a palindrome
+ * @s: the string to check
+ * @i: the first index to compare
+ * @j: the last index to compare
+ * Return: 1 if it is, 0 if not
+ */
+int check_pal(char *s, int i, int j)
 {
-    if (i >= len)
+    if (i >= j)
         return 1;
-    if (s[i] != s[len])
+    if (s[i] != s[j])
         return 0;
-    return _is_palindrome(s, i + 1, len - 1);
+    return check_pal(s, i + 1, j - 1);
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string to check
+ * Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+    if (*s == '\0')
+        return 0;
+    return 1 + _strlen_recursion(s + 1);
 }
