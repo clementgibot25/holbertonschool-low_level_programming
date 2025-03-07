70ddca79b3ef521aec17578ae6d46ac0631b5be5
clementgibot25 <10500@holbertonstudents.com>
2025-03-07 14:56:41 +0100

    added last exo

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -14,26 +14,26 @@ int main(int argc, char *argv[])
 	int sum = 0;
 	int num;
 
-	if (argc == 1)
-    {
-        printf("0\n");
-        return (0);
-    }
+	for (int i = 1; i < argc; i++)
+	{
+		int num = 0;
+		int j = 0;
 
-    for (i = 1; i < argc; i++)
-    {
-        for (j = 0; argv[i][j] != '\0'; j++)
-        {
-            if (argv[i][j] < '0' || argv[i][j] > '9')
-            {
-                printf("Error\n");
-                return (1);
-            }
-        }
-        sum += atoi(argv[i]);
-    }
+		while (argv[i][j] != '\0')
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			num = num * 10 + (argv[i][j] - '0');
+			j++;
+		}
 
-    printf("%d\n", sum);
-    return (0);
+		sum += num;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
 }
-
