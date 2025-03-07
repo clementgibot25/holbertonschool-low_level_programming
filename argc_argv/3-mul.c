f71a338cb191809b77d0af1193144595addb154a
clementgibot25 <10500@holbertonstudents.com>
2025-03-07 14:23:16 +0100

    added exo 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+
+	int result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
