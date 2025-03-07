dcba59ebdeb6cd80557e3e7a6e6cab71e6a959cc
clementgibot25 <10500@holbertonstudents.com>
2025-03-07 14:40:07 +0100

    added last exo

---
+++ argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+
+	for (int i = 1; i < argc; i++)
+	{
+		int num = 0;
+		int j = 0;
+
+		while (argv[i][j] != '\0')
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			num = num * 10 + (argv[i][j] - '0');
+			j++;
+		}
+
+		sum += num;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
