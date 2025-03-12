741fe84cb8ed60ce35877dda8a0d88ff12fe7533
clementgibot25 <10500@holbertonstudents.com>
2025-03-12 14:41:34 +0100

    fixing dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef STRUCTURES_TYPEDEF_DOG_H
 #define STRUCTURES_TYPEDEF_DOG_H
 
+/**
+* struct dog - structure about dog
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner name
+*/
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif
