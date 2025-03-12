615fa38e0b2398be1275c77598de63760bcc915d
clementgibot25 <10500@holbertonstudents.com>
2025-03-12 10:18:04 +0100

    added exo 2 updated main

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	memset(ptr, 0, nmemb * size);
+	return (ptr);
+}
