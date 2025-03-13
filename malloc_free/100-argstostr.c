2146e9aa1dd84f772e071ceb26d6c19ff89596e7
clementgibot25 <10500@holbertonstudents.com>
2025-03-13 13:49:52 +0100

    added advanced task

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -3,25 +3,31 @@
 #include "main.h"
 
 /**
- * argstostr - concatenates all the arguments of the program
- * @ac: number of arguments (type: int)
- * @av: array of arguments (type: char **)
+ * argstostr - concatenates all the arguments of the program.
+ * @ac: the number of arguments.
+ * @av: an array of strings representing the arguments.
  *
- * Return: pointer to a new string (type: char *), or NULL if it fails
+ * Return: a pointer to a new string, or NULL if ac == 0 or av == NULL,
+ * or if memory allocation fails. Each argument is followed by a newline
+ * character in the resulting string.
  */
+
 char *argstostr(int ac, char **av)
 {
-	int i, j, k;
+	int i, j, k, total_length = 0;
 	char *str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
-	for (j = 0; av[i][j] != '\0'; j++)
-		;
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			;
+		total_length += j + 1;
+	}
 
-	str = malloc(sizeof(char) * (i + j + 1));
+	str = malloc(sizeof(char) * (total_length + 1));
 	if (str == NULL)
 		return (NULL);
 
