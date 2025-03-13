efdbc9fd8aaf525f5748f0c9b407494830834135
clementgibot25 <10500@holbertonstudents.com>
2025-03-13 12:03:32 +0100

    fixing exo 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -25,10 +25,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->owner = malloc(strlen(owner) + 1);
 
 	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
 
-	new_dog->name = name;
 	new_dog->age = age;
-	new_dog->owner = owner;
+	strcpy(new_dog->name, name);
+	strcpy(new_dog->owner, owner);
 
 	return (new_dog);
 }
