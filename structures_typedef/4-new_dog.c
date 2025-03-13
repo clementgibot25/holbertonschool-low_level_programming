c45834ec6b2af123cb4968af351ed9903a436b26
clementgibot25 <10500@holbertonstudents.com>
2025-03-13 13:22:18 +0100

    updated dog.h

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,12 +3,48 @@
 #include "dog.h"
 
 /**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * _strlen - returns the length of a string
+ * @s: string to check
  *
- * Return: pointer to the new dog
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s && s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * _strcpy - copies a string to a destination
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to the destination string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - creates a new dog_t structure
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner's name of the dog
+ *
+ * Return: a pointer to the new dog_t structure,
+ * or NULL if memory allocation fails
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
@@ -21,8 +57,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new_dog->name = malloc(strlen(name) + 1);
-	new_dog->owner = malloc(strlen(owner) + 1);
+	new_dog->name = malloc(_strlen(name) + 1);
+	new_dog->owner = malloc(_strlen(owner) + 1);
 
 	if (new_dog->name == NULL || new_dog->owner == NULL)
 	{
@@ -32,8 +68,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
 	new_dog->age = age;
-	strcpy(new_dog->name, name);
-	strcpy(new_dog->owner, owner);
+	_strcpy(new_dog->name, name);
+	_strcpy(new_dog->owner, owner);
 
 	return (new_dog);
 }
