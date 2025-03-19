2c8c9a145867da5214edc3f5ac24c111e3e3509a
clementgibot25 <10500@holbertonstudents.com>
2025-03-19 15:56:22 +0100

    fixing the print function exo 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -5,11 +5,8 @@
  * print_name - prints a name
  * @name: the name to print
  * @f: the function pointer
- *
- * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		(*f)(name);
+	(*f)(name);
 }
