23d724adc50a93bdc4a52edffd03d7e1e157984e
clementgibot25 <10500@holbertonstudents.com>
2025-03-19 16:02:22 +0100

    updated the function that prints name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,11 @@
 #include "function_pointers.h"
-
 /**
- * print_name - prints a name
- * @name: the name to print
- * @f: the function pointer
+ * print_name - function that prints a name
+ * @name: string(name) to print
+ * @f: function callback used to print (as it is or with uppercases)
+ * @*: string(name) to print
  */
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+(*f)(name);
 }
