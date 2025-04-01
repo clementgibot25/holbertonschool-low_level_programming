9a0c63622d4954a48b635401f2848d6de5269bed
clementgibot25 <10500@holbertonstudents.com>
2025-04-01 15:20:39 +0200

    removed the strlen function

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,6 +11,10 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *last_node;
+	size_t len = 0;
+
+	while (str[len] != '\0')
+		len++;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -23,7 +27,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	new_node->len = len;
 	new_node->next = NULL;
 
 	if (*head == NULL)
