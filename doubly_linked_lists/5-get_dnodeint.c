8a92624410f7f796cceecadb01c7aae92bd79a3b
clementgibot25 <10500@holbertonstudents.com>
2025-04-02 14:57:35 +0200

    added functions about node insert and delete

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: head of the list
+ * @index: index of the node, starting from 0
+ * Return: the nth node of a dlistint_t linked list
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+	dlistint_t *current = head;
+
+	while (current != NULL)
+	{
+		if (count == index)
+			return (current);
+		current = current->next;
+		count++;
+	}
+
+	return (NULL);
+}
