9b9e584bcad005a2a9af16fe0ccfb1e57b0816b5
clementgibot25 <10500@holbertonstudents.com>
2025-04-03 11:36:33 +0200

    fixed betty error

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -10,21 +10,21 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0;
-    int i = 0;
+	unsigned int result = 0;
+	int i = 0;
 
-    if (b == NULL)
-        return (0);
+	if (b == NULL)
+		return (0);
 
-    while (b[i] != '\0')
-    {
-        if (b[i] != '0' && b[i] != '1')
-            return (0);
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 
-        result <<= 1;
-        result |= (b[i] - '0');
-        i++;
-    }
+		result <<= 1;
+		result |= (b[i] - '0');
+		i++;
+	}
 
-    return (result);
+	return (result);
 }
