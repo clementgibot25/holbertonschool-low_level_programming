f056a1c07ba49dbb65b55504bb31167fde685ecf
clementgibot25 <10500@holbertonstudents.com>
2025-04-04 12:01:01 +0200

    fixed function

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,30 +10,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+    int fd, w, len = 0;
 
-	if (filename == NULL)
-		return (-1);
+    if (filename == NULL)
+        return (-1);
 
-	if (text_content == NULL)
-	{
-		text_content = "";
-		len = 0;
-	}
-	else
-	{
-		len = strlen(text_content);
-	}
+    if (text_content != NULL)
+        len = strlen(text_content);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
+    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+    if (fd == -1)
+        return (-1);
 
-	w = write(fd, text_content, len);
-	if (w == -1)
-		return (-1);
+    w = write(fd, text_content, len);
+    if (w == -1)
+        return (-1);
 
-	close(fd);
+    close(fd);
 
-	return (1);
+    return (1);
 }
